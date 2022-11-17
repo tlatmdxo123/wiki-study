471f1b66805e48be2b0d9fa3958cca06b901b50b
tlatmdxo123 <tmdxosmswkd@gmail.com>
2022-11-17 14:47:41 +0900

    Feat : 임시 회원가입 api 구현

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+    await this.userService.create(name, email, password);
+  }
+}
