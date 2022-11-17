9464970fb37e62ff57ae62582b0b4211bdbd8f4c
tlatmdxo123 <tmdxosmswkd@gmail.com>
2022-11-17 16:25:48 +0900

    Refactor : 이메일 모듈 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { EmailService } from './email/email.service';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, EmailModule],
   controllers: [AppController],
   providers: [AppService, EmailService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { EmailService } from 'src/email/email.service';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
+  imports: [EmailModule],
   controllers: [UserController],
-  providers: [UserService, EmailService],
+  providers: [UserService],
 })
 export class UserModule {}
