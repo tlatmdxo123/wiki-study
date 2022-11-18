c269040f40a92e31e6fc1790e82e368b8a160d22
tlatmdxo123 <tmdxosmswkd@gmail.com>
2022-11-18 23:06:33 +0900

    Feat : user service에 user repository 적용

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { EmailModule } from 'src/email/email.module';
+import { UserEntity } from './entities/user.entity';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { EmailService } from 'src/email/email.service';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import * as uuid from 'uuid';
+import { ulid } from 'ulid';
+import { EmailService } from 'src/email/email.service';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  constructor(private emailService: EmailService) {}
+  constructor(
+    private emailService: EmailService,
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
 
   // async getUserInfo(userId: string): Promise<UserInfo> {
   //   //1.userId를 가진 유저가 존재하는지 DB에서 확인하고 없다면 에러처리
@@ -14,7 +22,12 @@ export class UserService {
   // }
 
   async createUser(name: string, email: string, password: string) {
-    await this.checkUserExists(email);
+    const userExist = await this.checkUserExists(email);
+    if (userExist) {
+      throw new UnprocessableEntityException(
+        '해당 이메일로는 가입할 수 없습니다',
+      );
+    }
 
     const signupVerifyToken = uuid.v1();
 
@@ -22,8 +35,10 @@ export class UserService {
     await this.sendMemberJoinEmail(email, signupVerifyToken);
   }
 
-  async checkUserExists(email: string) {
-    return false; //@Todo db연동 후 구현
+  async checkUserExists(emailAddress: string) {
+    const user = await this.userRepository.findOneBy({ email: emailAddress });
+
+    return user !== undefined;
   }
 
   async saveUser(
@@ -32,7 +47,14 @@ export class UserService {
     password: string,
     signupVerifyToken: string,
   ) {
-    return; //@Todo db연동 후 구현
+    const user = new UserEntity();
+    user.id = ulid();
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    user.signupVerifyToken = signupVerifyToken;
+
+    await this.userRepository.save(user);
   }
 
   async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
