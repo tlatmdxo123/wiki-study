9b037400afdaba2534cb2047feb66ef815175d76
tlatmdxo123 <tmdxosmswkd@gmail.com>
2022-11-18 22:12:10 +0900

    Feat : 유저 생성 dto에 validation pipe 추가

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,33 @@
+import { BadRequestException } from '@nestjs/common';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+
 export class CreateUserDto {
+  @Transform(({ value, obj }) => {
+    if (obj.password.includes(value.trim())) {
+      throw new BadRequestException(
+        'password는 name과 같은 문자열을 포함할 수 없습니다.',
+      );
+    }
+    return value.trim();
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   name: string;
+
+  @IsString()
+  @IsEmail()
+  @MaxLength(60)
   email: string;
-  password: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
+  readonly password: string;
 }
