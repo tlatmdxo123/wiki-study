49ee53a58b32bf498feb5c4d976162f0ead234d8
seungtae <seungtae@bejewel.kr>
2022-11-29 11:02:44 +0900

    Refactor : ExceptionFilter에 커스텀 logger사용 및 로그 내용에 stack 추가

--- src/exception/http-exception.filter.ts
+++ src/exception/http-exception.filter.ts
@@ -5,10 +5,12 @@ import {
   ExceptionFilter,
   HttpException,
   InternalServerErrorException,
+  Logger,
 } from '@nestjs/common';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private logger: Logger) {}
   catch(exception: any, host: ArgumentsHost): any {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
@@ -20,13 +22,16 @@ export class HttpExceptionFilter implements ExceptionFilter {
 
     const response = (exception as HttpException).getResponse();
 
+    const stack = exception.stack;
+
     const log = {
       timestamp: new Date(),
       url: req.url,
       response,
+      stack,
     };
 
-    console.log(log);
+    this.logger.log(log);
 
     res.status((exception as HttpException).getStatus()).json(response);
   }
