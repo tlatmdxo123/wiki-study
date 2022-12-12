715e64cc7f22ed3053f1a0feb0c8d1245c7a36e9
tlatmdxo123 <tmdxosmswkd@gmail.com>
2022-12-12 22:10:11 +0900

    Refactor : verify-email handler에 repository 적용

--- src/user/command/verify-email.handler.ts
+++ src/user/command/verify-email.handler.ts
@@ -1,32 +1,32 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
 import { VerifyEmailCommand } from './verify-email.command';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UserEntity } from '../entities/user.entity';
-import { Repository } from 'typeorm';
 import { AuthService } from '../../auth/auth.service';
+import { UserRepository } from '../infra/db/repository/UserRepository';
 
 @Injectable()
 @CommandHandler(VerifyEmailCommand)
 export class VerifyEmailHandler implements ICommandHandler<VerifyEmailCommand> {
   constructor(
-    @InjectRepository(UserEntity)
-    private userRepository: Repository<UserEntity>,
+    @Inject('UserRepository') private userRepository: UserRepository,
     private authService: AuthService,
   ) {}
 
   async execute(command: VerifyEmailCommand) {
     const { signupVerifyToken } = command;
-    const user = await this.userRepository.findOneBy({ signupVerifyToken });
+    const user = await this.userRepository.findBySignupVerifyToken(
+      signupVerifyToken,
+    );
 
     if (!user) {
       throw new NotFoundException('유저가 존재하지 않습니다');
     }
 
     return this.authService.login({
-      id: user.id,
-      name: user.name,
-      email: user.email,
+      id: user.getId(),
+      name: user.getName(),
+      email: user.getEmail(),
     });
   }
 }
