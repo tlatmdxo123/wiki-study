686284fc9dbc1c1f0955ab82912d7ac3be354dae
tlatmdxo123 <tmdxosmswkd@gmail.com>
2022-12-12 22:08:11 +0900

    Refactor : login handler에 repository 적용

--- src/user/command/login.handler.ts
+++ src/user/command/login.handler.ts
@@ -1,32 +1,33 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
 import { LoginCommand } from './login.command';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UserEntity } from '../entities/user.entity';
-import { Repository } from 'typeorm';
 import { AuthService } from '../../auth/auth.service';
+import { UserRepository } from '../infra/db/repository/UserRepository';
 
 @Injectable()
 @CommandHandler(LoginCommand)
 export class LoginHandler implements ICommandHandler<LoginCommand> {
   constructor(
-    @InjectRepository(UserEntity)
-    private userRepository: Repository<UserEntity>,
+    @Inject('UserRepository') private userRepository: UserRepository,
     private authService: AuthService,
   ) {}
 
   async execute(command: LoginCommand): Promise<any> {
     const { email, password } = command;
-    const user = await this.userRepository.findOneBy({ email, password });
+    const user = await this.userRepository.findByEmailAndPassword(
+      email,
+      password,
+    );
 
     if (!user) {
       throw new NotFoundException('유저가 존재하지 않습니다');
     }
 
     return this.authService.login({
-      id: user.id,
-      name: user.name,
-      email: user.email,
+      id: user.getId(),
+      name: user.getName(),
+      email: user.getEmail(),
     });
   }
 }
